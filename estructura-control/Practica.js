048d5c353e0dd80714589dddcf0fcd131342a75b
MarianoEspinoza1022 <mario240602@gmail.com>
2024-07-01 22:59:11 -0600

    Add files via upload

---
+++ estructura-control/Practica.js
@@ -0,0 +1,11 @@
+//Realizar un que indique si un numero es positivo o negativo
+
+let result=1;
+
+if (num>0){
+    console.log("es positivo");
+}else if (num<0){
+    console.log("es negativo");
+} else{
+    console.log("es cero");
+}
---
+++ estructura-control/if_else+if_else.js
@@ -0,0 +1,19 @@
+// if else + if else
+
+let q1=10;
+
+if(q1===10){
+    console.log("Buen tarbajo");
+}else if (q1===9){
+    console.log("Casi perfecto");
+}else if(q1===8){
+    console.log("Bien supongo");
+}else if(q1===7){
+    console.log("Esfuerzate");
+}else if(q1===6){
+    console.log("Estudia un poco más");
+}else if(q1===5){
+    console.log("Mejor en ING que 10 LIC");
+}else{
+    console.log("Syntax error");
+}
